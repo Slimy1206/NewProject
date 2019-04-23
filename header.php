a7564d17e55f81c8b5a404519a1ffc05a5c2b32e
Ernb4s <48019701+Ernb4s@users.noreply.github.com>
2019-04-23 04:37:40 +0200

    semicolon fix

--- header.php
+++ header.php
@@ -20,13 +20,13 @@
 						if (isset($SESSION['u_id'])) {
 							echo '<form action="includes/logout.inc.php" method="POST">
 								<button type="submit" name="submit">Logout>/button>
-						</form>'
+						</form>';
 						}else {
 							echo '<form action="includes/login.inc.php" method="POST">
 							<input type="text" name="uid" placeholder="Username/e-mail">
 							<input type="password" name="pwd" placeholder="password">
 							<button type="submit" name="submit">Login</button>
-						</form>'
+						</form>';
 						}
 					?>
 					<a href="signup.php">Sign up</a>
