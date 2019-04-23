9a04e4de9766d1838a0a71783f3c7d06ca22e7d3
Ernb4s <48019701+Ernb4s@users.noreply.github.com>
2019-04-23 04:56:15 +0200

    Aktueller Stand 'Alpha'

--- index.php
+++ index.php
@@ -5,6 +5,11 @@
 	<section class="main-container">
 		<div class="main-wrapper">
 			<h2>In Arbeit! Versuchen sie es zu einem späteren Zeitpunkt erneut <3</h2>
+			<?php
+				if (isset($SESSION['u_id'])){
+					echo "You sold your soul! Muahahahahahaha!!!";
+				}
+			?>
 		</div>
 	</section>
 	
